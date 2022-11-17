f2a29cb38846fa3b5ec5a8df5e0e67db12da284b
mechupe <elizaaarov@mail.ru>
2022-11-17 22:07:02 +0300

    add function, which saves most common pixels value to the file

--- src/data_process/data_processer.py
+++ src/data_process/data_processer.py
@@ -1,18 +1,10 @@
-import sys
+import os
 from collections import Counter
 import cv2
-import numpy as np
-from cloudinary import CloudinaryImage
-import cloudinary.uploader
-import cloudinary
-import face_recognition
-import cloudinary.api
-import matplotlib.pyplot as plt
-from PIL import Image
 import json
 
 # Find background colors
-threshhold = 40
+background_threshold = 40
 
 
 def count_colors(image_path):
@@ -22,8 +14,8 @@ def count_colors(image_path):
 
     # Flattens the 2D single channel array so as to make it easier to iterate over it
     channel_b = channel_b.flatten()
-    channel_g = channel_g.flatten()  # ""
-    channel_r = channel_r.flatten()  # ""
+    channel_g = channel_g.flatten()
+    channel_r = channel_r.flatten()
     rgb_list = list()
     for i in range(len(channel_b)):
         pixel_rgb = (channel_r[i], channel_g[i], channel_b[i])
@@ -34,22 +26,60 @@ def count_colors(image_path):
 
 def percentile_colors(image_path):
     counts_colors = count_colors(image_path).most_common()
-
     counts = [count[1] for count in counts_colors]
-    print(counts)
     perc_counts = [count / sum(counts) for count in counts]
 
     iterator = 0
     summator = 0
+
     for percent in perc_counts:
-        if summator < threshhold:
+        if summator < background_threshold:
             summator += percent * 100
             iterator += 1
         else:
             return [rgb for rgb, count in counts_colors[:iterator]]
 
-if __name__ == "__main__":
-    print(percentile_colors('17357.jpg'))
+
+def background_pixels_define(data_path, threshold, ):
+
+    os.chdir(data_path)
+    # We count images in order to stop loop when limit the threshold
+    counter = 0
+    man_woman = os.listdir(os.getcwd())
+    most_pixels = list()
+    # Go to dir with folders MEN WOMEN
+    for sex in man_woman:
+        sex_path = os.getcwd()
+        os.chdir(sex_path + f'\\{sex}')
+        categories = os.listdir(os.getcwd())
+        # Go to category of clothes
+        for category in categories:
+            categoties_path = os.getcwd()
+            os.chdir(categoties_path + f'\\{category}')
+            images = os.listdir(os.getcwd())
+            # Go to image
+            for image in images:
+                most_pixels.extend(percentile_colors(image_path=image))
+                counter += 1
+                print(f'{threshold - counter} files left')
+                # Break if hit threshold
+                if counter > threshold:
+                    break
+            if counter > threshold:
+                # Reset counter and go to the next gender dir
+                counter = 0
+                break
+            os.chdir(categoties_path)
+        os.chdir(sex_path)
+    # Count most common pixels and save info to the file
+    backgrounds = Counter(most_pixels)
+    with open('backgrounds.json', 'w') as file:
+        backgrounds = json.dumps({str(k): backgrounds[k] for k in backgrounds})
+        json.dump(backgrounds, file)
+
+
+#if __name__ == "__main__":
+
 
 # if __name__ == '__main__':
 #     src = cv2.imread('3757.jpg')
