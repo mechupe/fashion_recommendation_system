ef37a9973b814bf4ef4061ad93a99cb75b46f8d1
mechupe <elizaaarov@mail.ru>
2023-04-05 00:46:20 +0300

    fixed bug in dropping duplicates, add height files moving function

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,10 +1,13 @@
 import os
+import shutil
 import pandas as pd
 from tqdm import tqdm
 from PIL import Image
 
-
 dir_name = r'C:\Users\79175\fashion_recommendation_system\data\Women'
+class_1_path = r"C:\Users\79175\fashion_recommendation_system\data\Ростовые"
+class_2_path = r"C:\Users\79175\fashion_recommendation_system\data\Неростовые"
+
 
 
 def quantity_inside_folder(root_dir, threshold=None):
@@ -38,7 +41,7 @@ def file_to_label(root_dir):
         for item in tqdm(os.listdir(dir_name + fr'\{folder}')):
             item_label_row = pd.DataFrame({'Артикул': [item.split(' _')[0]], 'Class': [folder]})
             item_label_df = pd.concat([item_label_df, item_label_row])
-    item_label_df.to_csv('item_label_df.csv')
+    item_label_df.to_csv('item_label_df_1.csv')
     return item_label_df
 
 
@@ -48,18 +51,42 @@ def drop_duplicates(root_dir, threshold):
     item_label_dict = {}
     for folder in tqdm(classes_list):
         for item in tqdm(os.listdir(dir_name + fr'\{folder}')):
-            item_series_num = item.split(' _')[0]
-            if item_series_num in item_label_dict:
+            # item_series_num = item.split(' _')[0]
+            if item in item_label_dict:
                 os.remove(dir_name + fr'\{folder}' + fr'\{item}')
             else:
-                item_label_dict[item_series_num] = folder
+                item_label_dict[item] = folder
 
 
 def to_8_bit(img_path):
     img = Image.open(img_path)
     img = img.convert('P', palette=Image.ADAPTIVE)
     # img = img.convert("RGB")
-                      # , palette=Image.ADAPTIVE, colors=8)
+    # , palette=Image.ADAPTIVE, colors=8)
     img.save('8_bit_1.png')
 
-to_8_bit('MP002XW0C77R _1.jpg')
+
+def files_moving(folder, class_1_path, class_2_path, root_dir):
+    for item in tqdm(os.listdir(root_dir + fr'\{folder}')):
+        item_num = item.split('.')[0][-1]
+        if item_num == '2':
+            shutil.copy(root_dir + fr'\{folder}' + '\\' + item,
+                        class_1_path)
+        elif item_num == '1':
+            shutil.copy(root_dir + fr'\{folder}' + '\\' + item,
+                        class_2_path)
+
+
+def separate_height_not_height_figures(root_dir):
+    folders = os.listdir(root_dir)
+    print(folders)
+    for folder in tqdm(folders):
+        if folder not in ['Комбинезон', 'Костюм', 'Футболка']:
+            files_moving(folder=folder, class_1_path=class_1_path, class_2_path=class_2_path, root_dir=root_dir)
+        elif folder in ['Комбинезон', 'Костюм']:
+            files_moving(folder=folder, class_1_path=class_2_path, class_2_path=class_1_path, root_dir=root_dir)
+        else:
+            continue
+
+
+# separate_height_not_height_figures(dir_name)
